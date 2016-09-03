e230347475f205811876ae8d4f630d01e72e1dac
jrozansk <jakub.rozanski@intel.com>
2016-09-03 23:45:53 +0100

    Possibility to count connected clients

--- poker_server.js
+++ poker_server.js
@@ -1,21 +1,36 @@
 var express = require('express');
 var app = express();
 
+const EventEmitter = require('events');
 const port = 3000;
 
+class MyEmitter extends EventEmitter {}
+
+var numberOfClients = 0;
+var myEmitter = new MyEmitter();
+
 app.set('view engine', 'pug');
+
 app.get('/', function(req, res) {
 	res.render('index');
 });
 
 app.get('/clients', function(req, res) {
-        //req.socket.setTimeout(Infinity);
         res.writeHead(200, {
           'Content-Type': 'text/event-stream',
           'Cache-Control': 'no-cache',
           'Connection': 'keep-alive'
         });
-        res.write('data:jrozansk\n\n');
+
+        myEmitter.on('join', () => {
+          res.write('data: ' + numberOfClients + '\n\n');
+        });
+});
+
+app.get('/join', function(req, res) {
+        numberOfClients += 1;
+        myEmitter.emit('join');
+        res.sendStatus(200)
 });
 
 app.listen(port);
