49fdb697f4b11ef8a0e8c74295381ed5f28ce6e1
jrozansk <jakub.rozanski@intel.com>
2016-09-04 21:17:08 +0100

    Activate voting pane when planning director fires event.

--- poker_server.js
+++ poker_server.js
@@ -30,7 +30,33 @@ app.get('/clients', function(req, res) {
 app.get('/join', function(req, res) {
         numberOfClients += 1;
         myEmitter.emit('join');
-        res.sendStatus(200)
+        res.append('Connection', 'close');
+        res.redirect('/voting_pane');
+});
+
+app.get('/voting_pane', function(req, res) {
+        res.render('vote');
+});
+
+app.get('/vote_enabled', function(req, res) {
+        res.writeHead(200, {
+          'Content-Type': 'text/event-stream',
+          'Cache-Control': 'no-cache',
+          'Connection': 'keep-alive'
+        });
+
+        myEmitter.on('activate', () => {
+          res.write('data: true\n\n');
+        });
+});
+
+app.post('/activate', function(req, res) {
+        myEmitter.emit('activate');
+        res.sendStatus(200);
+});
+
+app.get('/dashboard', function(req, res) {
+        res.render('dashboard');
 });
 
 app.listen(port);
