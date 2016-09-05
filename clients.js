754ba4cb4241e41c38c7c56a3846a425f1b64d30
jrozansk <jakub.rozanski@intel.com>
2016-09-05 18:56:09 +0100

    Cleaning everything up before moving to socket.io This action is dictated by really low possibility to implement client disconnect event based on SSE.

--- clients.js
+++
@@ -1,7 +0,0 @@
-if(!!window.EventSource) {
-  var source = new EventSource('/clients')
-
-  source.onmessage = function(e) {
-    document.body.innerHTML += e.data + '<br />';
-  }
-}
--- poker_server.js
+++ poker_server.js
@@ -6,8 +6,8 @@ const port = 3000;
 
 class MyEmitter extends EventEmitter {}
 
-var numberOfClients = 0;
 var myEmitter = new MyEmitter();
+var numberOfClients = 0;
 
 app.set('view engine', 'pug');
 
@@ -15,44 +15,8 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
-app.get('/clients', function(req, res) {
-        res.writeHead(200, {
-          'Content-Type': 'text/event-stream',
-          'Cache-Control': 'no-cache',
-          'Connection': 'keep-alive'
-        });
-
-        myEmitter.on('join', () => {
-          res.write('data: ' + numberOfClients + '\n\n');
-        });
-});
-
-app.get('/join', function(req, res) {
-        numberOfClients += 1;
-        myEmitter.emit('join');
-        res.append('Connection', 'close');
-        res.redirect('/voting_pane');
-});
-
-app.get('/voting_pane', function(req, res) {
-        res.render('vote');
-});
-
-app.get('/vote_enabled', function(req, res) {
-        res.writeHead(200, {
-          'Content-Type': 'text/event-stream',
-          'Cache-Control': 'no-cache',
-          'Connection': 'keep-alive'
-        });
-
-        myEmitter.on('activate', () => {
-          res.write('data: true\n\n');
-        });
-});
-
-app.post('/activate', function(req, res) {
-        myEmitter.emit('activate');
-        res.sendStatus(200);
+app.get('/client', function(req, res) {
+        res.render('client');
 });
 
 app.get('/dashboard', function(req, res) {
