49707c15029c12bf7675d24993aa4606ec05ef5d
jrozansk <jakub.rozanski@intel.com>
2016-09-08 10:14:44 +0100

    Determining whether client can vote (even after webpage reload) - introduced express-session - introduced stylesheet - client quantity determined by counting unique session IDs

--- poker_server.js
+++ poker_server.js
@@ -1,44 +1,58 @@
 var express = require('express');
 var io = require('socket.io');
 var app = express();
+var session = require("express-session");
 
 const EventEmitter = require('events');
 const port = 3000;
 
-class MyEmitter extends EventEmitter {}
-
-var myEmitter = new MyEmitter();
-var realClients = [];
+var clientsConnected = new Set();
+var isPlanningActive = false;
+var answers = {};
+var sessionMiddleware = session({
+    'secret' : Math.random().toString(36).slice(-8),
+    'resave' : false,
+    'saveUninitialized' : true
+});
 
 app.set('view engine', 'pug');
+app.use(express.static('stylesheets'));
+app.use(sessionMiddleware);
 
 app.get('/', function(req, res) {
 	res.render('index');
 });
 
 app.get('/client', function(req, res) {
-    res.render('client');
+    var votingNeeded = isPlanningActive;
+    if(req.session.id in answers) {
+        votingNeeded = false;
+    }
+    res.render('client', {votingNeeded: votingNeeded});
 });
 
 app.get('/dashboard', function(req, res) {
-    res.render('dashboard', {clientsQuantity: realClients.length});
+    res.render('dashboard', {clientsQuantity: clientsConnected.length});
 });
 
 var server = app.listen(port);
+
 sockets = io.listen(server, {'pingInterval': 2000, 'pingTimeout': 5000});
+sockets.use(function(socket, next) {
+    sessionMiddleware(socket.request, socket.request.res, next);
+});
 
 sockets.on('connection', function(socket) {
-   var dataPassed = socket.request;
-   if(dataPassed._query['isItRealClient'] == 'true') {
-       realClients.push(dataPassed._query['t'])
-       sockets.emit('clientsQuantityChange', {'quantity': realClients.length});
-   }
+    console.log(socket.request.session.id);
+    clientsConnected.add(socket.request.session.id)
+    sockets.emit('clientsQuantityChange', {'quantity': clientsConnected.size});
 
-   socket.on('disconnect', function() {
-     socketPos = realClients.indexOf(dataPassed._query['t']);
-     if(socketPos != -1) {
-        realClients.splice(socketPos, 1)
-        sockets.emit('clientsQuantityChange', {'quantity': realClients.length}); 
-     }
-   });
+    socket.on('disconnect', function() {
+      clientsConnected.delete(socket.request.session.id)
+      sockets.emit('clientsQuantityChange', {'quantity': clientsConnected.size}); 
+    });
+    
+    socket.on('activate', function() {
+      socket.broadcast.emit('activate');
+    });
 });
